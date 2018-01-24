c3326ec364278567a3e8a304de037487b9f77e05
ramyaavak <35220550+ramyaavak@users.noreply.github.com>
2018-01-24 16:44:16 +0530

    prime.py
    prime

--- prime.py
+++ prime.py
@@ -1,5 +1,9 @@
-lower=1
-upper=10
-for i in range(lower,upper+1):
-    if(i%2!=0):
-       print(i)
+lower = 1
+upper = 10
+for num in range(lower,upper + 1):
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
